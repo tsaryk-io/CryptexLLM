cf2161e0ef4fbb98f2c99f0cb50a404eef4ff044
marytsaryk <mary.tsaryk@gmail.com>
2025-07-23 14:54:49 -0500

    Remove mixed precision to avoid DeepSpeed conflicts

--- launch_experiment.py
+++ launch_experiment.py
@@ -154,7 +154,6 @@ def launch_experiment(args):
     cmd = [
         'accelerate', 'launch',
         '--multi_gpu',
-        '--mixed_precision', 'fp16',
         '--num_processes', static_config['num_process'],
         '--main_process_port', static_config['master_port'],
         'run_main.py',
